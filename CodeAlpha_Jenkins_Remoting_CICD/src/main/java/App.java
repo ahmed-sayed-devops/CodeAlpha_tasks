07ae575cd2fbb5cf2fbb6a5108c404d7905b2889
Ahmed Sayed <ahmed.s88.devops@gmail.com>
2026-03-09 00:04:23 +0200

    first commit

---
+++ CodeAlpha_Jenkins_Remoting_CICD/src/main/java/App.java
@@ -0,0 +1,16 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("CodeAlpha DevOps Internship Task");
+        System.out.println("Jenkins CI/CD Pipeline Demo");
+
+        System.out.println("-----------------------------");
+
+        System.out.println("Application built and deployed successfully.");
+
+        System.out.println("Author: Ahmed Sayed Abd Elbaseer");
+
+    }
+
+}
